caae9ad3c324e9adb9fb915330e6a08d44db3b24
Azeez Adebayo <debaycisse@gmail.com>
2024-06-01 09:31:30 +0100

    implement task 2. Select some Amenities
    filters some amenities and display them under Amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,29 @@
 #!/usr/bin/node
 /* Houses jquery api that scripts the frontend of a flask app, defined in 1-hbnb.py, inside web_dynamic directory */
 $(document).ready(function () {
-  $('.popover ul li input').click(function () {
-    if (this.checked) {
-      
-    }
-    $('.amenities h4').append(this['data-name']);
+  const filters = [];
+  $.each($('.popover ul li input'), function (ind, val) {
+    $(val).click(function () {
+      const h4Element = $('DIV.amenities H4');
+      let texts = '';
+      if ($(val).is(':checked')) {
+        filters.push($(this).attr('data-name'));
+      } else {
+        filters.splice(filters.indexOf($(this).attr('data-name')), 1);
+      }
+      $.each(filters, function (index, value) {
+        if (texts.length < 2) {
+          texts = texts.concat(value);
+        } else {
+          texts = texts.concat(', ');
+          texts = texts.concat(value);
+        }
+      });
+      if (filters.length < 1) {
+        h4Element.html('&nbsp;');
+      } else {
+        h4Element.text(texts);
+      }
+    });
   });
 });
